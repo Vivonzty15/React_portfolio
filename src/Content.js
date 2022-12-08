2459d0bc27a96c379b23b96409d95fb8dca93772
Braxton Allen <braxtonallen99@yahoo.com>
2022-12-07 23:18:03 -0600

    build issues

--- src/Content.js
+++ src/Content.js
@@ -17,10 +17,10 @@ import project2 from "./assets/images/projects/img2.png";
 import project3 from "./assets/images/projects/img3.png";
 import person_project from "./assets/images/projects/person.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
+//import avatar1 from "./assets/images/Testimonials/avatar1.png";
+//import avatar2 from "./assets/images/Testimonials/avatar2.png";
+//import avatar3 from "./assets/images/Testimonials/avatar3.png";
+//import avatar4 from "./assets/images/Testimonials/avatar4.png";
 
 import Hireme_person from "./assets/images/Hireme/person.png";
 
